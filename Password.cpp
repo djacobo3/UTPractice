15effe4735ab7264d43ef7152f1a84d810a25c1d
DavidJ <djacobo3@csuchico.edu>
2024-04-15 11:51:19 -0700

    Implement has_mixed_case

--- Password.cpp
+++ Password.cpp
@@ -1,5 +1,6 @@
 #include "Password.h"
 #include <string>
+#include <cctype>
 
 using std::string;
 
@@ -17,4 +18,25 @@ int Password::count_leading_characters(string phrase){
     index++;
   }
   return repetition;
+}
+
+/*
+  receives a string and returns whether it has both at least one upper-case
+  letter and at least one lower-case letter
+*/
+bool Password::has_mixed_case(string phrase){
+  bool upper_check = false;
+  bool lower_check = false;
+  for (int i = 0; i < phrase.length(); i++){
+    if (isupper(phrase[i])){
+      upper_check = true;
+    }
+    else if (islower(phrase[i])){
+      lower_check = true;
+    }
+    if (upper_check == true && lower_check == true){
+    return true;
+    }
+  }
+  return false;
 }
